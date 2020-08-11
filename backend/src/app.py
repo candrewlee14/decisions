1d7f1b94b7ab0ff56d1ad5f66da80f7e36c7c050
Andrew Lee <candrewlee14@gmail.com>
2020-08-10 23:26:46 -0600

    add angular frontend boilerplate

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import OpenSSL.debug
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import null
@@ -14,9 +15,10 @@ from src.models.decision import Decision, DecisionSchema
 
 # creating the Flask application
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db/decisions.db'
-app.config['SQLALCHEMY_ECHO'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
 with app.app_context():
